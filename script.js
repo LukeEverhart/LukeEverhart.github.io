921129179b739e5f6b004aef0d3661f0d22b09a8
LukeEverhart <82460467+LukeEverhart@users.noreply.github.com>
2021-04-26 19:12:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,97 +1,118 @@
-const dragDrop = require('drag-drop')
-const WebTorrent = require('webtorrent')
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Webtorrent Video Player</title>
+    </head>
+    <body>
+        <h1>Download files using the WebTorrent protocol (BitTorrent over WebRTC).</h1>
+		
+	<div class="switch">Dark mode:
+            <button id="inner-switch">Toggle Dark</span>
+        </div>
 
- 
+    <form>
+      <label for="torrentId">Download from a magnet link: </label>
+      <input name="torrentId" type="text" placeholder="magnet:" value="magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent">
+      <button type="submit">Download</button>
+	  
+    </form>
+	<div id="drop_zone" >
+        <center>
+            <p style="inline-size: 50%;">Submit File to Seed</p>
+            <img id="image" src="submit.png">
+        </center>
+      </div>
+	
+	
+	<hl>Copy the MagnetURI to share this video.</h1>
+	<div class="uri"></div>
+	
+	<h2>Download Progress</h2>
+	<div class="progress"></div>
+	
+	<div id="myProgress">
+        <div id="myBar"></div>
+    </div>
+	
+	<h3>Torrent Data</h3>
+	<div class="torrentData"></div>
+	
+	<div class="peers"></div>
+	
+			
+		
+    </body>
+    <script src="bundle.js">
 
- 
+    </script>
+	
+	<style>
+	
+	button {
+    color: #444444;
+    background: #F3F3F3;
+    border: 1px #DADADA solid;
+    padding: 12px 20px;
+    margin: 8px 0;
+    border-radius: 2px;
+    font-weight: bold;
+    font-size: 9pt;
+    outline: none;
+	}
 
- 
+	button:hover {
+    border: 1px #C6C6C6 solid;
+    box-shadow: 1px 1px 1px #EAEAEA;
+    color: #333333;
+    background: #F7F7F7;
+	}
 
+	button:active {
+    box-shadow: inset 1px 1px 1px #DFDFDF;
+	}
 
-if (WebTorrent.WEBRTC_SUPPORT) {
-  console.log("WebRTC Supported")
-} else {
-  console.log("WebRTC Not Supported")
-}
+	input[type=text] {
+    padding: 12px 20px;
+    margin: 8px 0;
+    box-sizing: border-box;
+    border: 2px solid red;
+    border-radius: 4px;
+	}
+	
+	#myProgress {
+    width: 100%;
+    background-color: grey;
+	}
 
- 
+	#myBar {
+    width: 1%;
+    height: 30px;
+    background-color: green;
+	}
+	
+	body {
+    padding: 25px;
+    background-color: white;
+    color: black;
+    font-size: 25px;
+  }
 
- 
+  .dark-mode {
+    background-color: #223838;
+    color: #DCDCDC;
+  }
 
- 
-
-
-var client = new WebTorrent()
-
-
-// When user drops files on the browser, create a new torrent and start seeding it!
-dragDrop('body', function (files) {
-  client.seed(files, function (torrent) {
-    console.log('Client is seeding ' + torrent.magnetURI)
-   
-    torrent.files[0].appendTo('body')
-  })
-})
- 
-
-      client.on('error', function (err) {
-        console.error('ERROR: ' + err.message)
-      })
-
- 
-
-      document.querySelector('form').addEventListener('submit', function (e) {
-        e.preventDefault() // Prevent page refresh
-
- 
-
-        var torrentId = document.querySelector('form input[name=torrentId]').value
-        log('Adding ' + torrentId)
-        client.add(torrentId, onTorrent)
-      })
-
- 
-
-      function onTorrent (torrent) {
-        log('Got torrent metadata!')
-        log(
-          'Torrent info hash: ' + torrent.infoHash + ' ' +
-          '<a href="' + torrent.magnetURI + '" target="_blank">[Magnet URI]</a> ' +
-          '<a href="' + torrent.torrentFileBlobURL + '" target="_blank" download="' + torrent.name + '.torrent">[Download .torrent]</a>'
-        )
-
- 
-
-        // Print out progress every 5 seconds
-        var interval = setInterval(function () {
-          log('Progress: ' + (torrent.progress * 100).toFixed(1) + '%')
-        }, 5000)
-
- 
-
-        torrent.on('done', function () {
-          log('Progress: 100%')
-          clearInterval(interval)
-        })
-
- 
-
-        // Render all files into to the page
-        torrent.files.forEach(function (file) {
-          file.appendTo('.log')
-          log('(Blob URLs only work if the file is loaded from a server. "http//localhost" works. "file://" does not.)')
-          file.getBlobURL(function (err, url) {
-            if (err) return log(err.message)
-            log('File done.')
-            log('<a href="' + url + '">Download full file: ' + file.name + '</a>')
-          })
-        })
-      }
-
- 
-
-      function log (str) {
-        var p = document.createElement('p')
-        p.innerHTML = str
-        document.querySelector('.log').appendChild(p)
-      }
+  #drop_zone {
+    border: 1px solid #223838;
+    width:  600px;
+    height: 400px;
+  }
+  #image{
+    width: 20%;
+    height: 20%;
+    opacity: 0.25;
+  }
+	</style>
+  
+</html>
